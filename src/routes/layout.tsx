d726a03ab94f7b08645a649c695b08c4838c5d6e
Daniela Botu <150161543+danabananaunu@users.noreply.github.com>
2026-01-10 13:19:07 +0200

    Update layout.tsx

--- src/routes/layout.tsx
+++ src/routes/layout.tsx
@@ -3,8 +3,8 @@ import { component$, Slot } from '@builder.io/qwik';
 export default component$(() => {
   return (
     <main style={{ maxWidth: '900px', margin: '0 auto', padding: '20px' }}>
-      <h1>Qwik Notes App</h1>
       <Slot />
     </main>
   );
 });
+
